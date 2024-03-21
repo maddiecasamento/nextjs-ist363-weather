104274451d7fdbe101965e1d451329ccb7058ac3
maddiecasamento <119361341+maddiecasamento@users.noreply.github.com>
2024-03-21 18:27:42 -0400

    attendence 3/21/24

--- app/page.js
+++ app/page.js
@@ -1,4 +1,13 @@
+import Button from "../components/Button";
+
 const Homepage = () => {
-  return <div>Whooo</div>;
+  return (
+  <div>
+    <h1>Weather app</h1>
+    <Button label="Download" />
+    <Button label="Register now"/>
+    <Button label="Learn" />
+  </div>
+  );
 };
 export default Homepage;
