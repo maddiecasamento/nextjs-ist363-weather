aa5569efacf89ce990f195e9d79a4e3dee213255
maddiecasamento <119361341+maddiecasamento@users.noreply.github.com>
2024-03-26 18:23:49 -0400

    attendence 3/26/24 React and useState

--- app/page.js
+++ app/page.js
@@ -1,12 +1,45 @@
+"use client";
+import { useState } from "react";
+
 import Button from "../components/Button";
+import ColorPicker from "../components/ColorPicker";
 
 const Homepage = () => {
+  const [count, setCount] = useState(0);
+  const [isVisible, setIsVisible] = useState();
+
   return (
   <div>
     <h1>Weather app</h1>
+    <h2>Count: {count}</h2>
+    <Button 
+      label="Increment"
+      clickHandler={() => {
+      setCount(count + 1);
+    }}
+    />
+      <Button 
+      label="Decrement"
+      clickHandler={() => {
+      setCount(count - 1);
+    }}
+    />
     <Button label="Download" />
     <Button label="Register now"/>
     <Button label="Learn" />
+
+      {count > 5 && <div>Special announcement message </div>}
+
+      <br />
+      <Button 
+      label={isVisible ? "Hide message" : "Show message"}
+      clickHandler={() => {
+        setIsVisible(!isVisible);
+      }}
+      />
+      {isVisible && <p>Hello, world!</p>}
+
+      <ColorPicker/>
   </div>
   );
 };
