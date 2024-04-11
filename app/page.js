ac025efa00098c8c5b673d62e2fa422722d21e56
maddiecasamento <119361341+maddiecasamento@users.noreply.github.com>
2024-04-11 18:22:48 -0400

    attendence 04/11/24

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import ButtonDemo from "../components/ButtonDemo";
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "../components/PeoplePicker";
+import Tabs from "../components/Tabs";
 
 import { getPeople, getWeatherData, getGeoLocation, getWeatherDataByLatLon } from "../lib/api";
 
@@ -80,15 +81,16 @@ const Homepage = () => {
     {/*<PeoplePicker people={peopleArr} />
     <ButtonDemo />
   <ColorPicker/>*/}
-  {daysOfWeek && (<section>
-  <ul>
-    {daysOfWeek?.map((day, index) => {
-      return <li key={index}>{day}</li>;
-    })}
-  </ul>
+  {daysOfWeek && (
+  <section>
+  <Tabs 
+  activeIndex={activeDayIndex}
+  items={daysOfWeek} 
+  clickHandler={setActiveDayIndex}/>
   <div>
     {weatherData?.list.filter((block) => {
     const date = new Date(block.dt * 1000);
+    const options = {weekday: "short"};
     const day = date.toLocaleDateString("en-US", options);
     return day === daysOfWeek[activeDayIndex];
   })
